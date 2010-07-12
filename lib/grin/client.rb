d0f354ad6088ce2a3dabc1adae5d10f1db0fbf97
James Miller <bensie@gmail.com>
2010-07-11 18:34:53 -0700

    Create albums

--- lib/grin/client.rb
+++ lib/grin/client.rb
@@ -20,10 +20,20 @@ module Grin
       end
     end
     
+    def create_album(title, category_id)
+      if album = post("albums.json", { :album => { :title => title, :category_id => category_id } })
+        return Album.new(album, @auth_string)
+      end
+    end
+    
     private
   
-    def get(path, ssl = false)
+    def get(path)
       JSON.parse(RestClient.get("http://#{@auth_string}.fotogger.com/api/#{API_VERSION}/#{path}").body)
+    end
+    
+    def post(path, params = {})
+      JSON.parse(RestClient.post("http://#{@auth_string}.fotogger.com/api/#{API_VERSION}/#{path}", params).body)
     end
     
   end
